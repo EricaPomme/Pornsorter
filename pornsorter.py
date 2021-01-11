1075c8f8e4af9a04c30f9655e5abbbf784b76ecd
Erica Young <erica@ejyoung.ca>
2021-01-10 16:40:17 -0800

    actually did stuff

--- pornsorter.py
+++ pornsorter.py
@@ -1,59 +1,119 @@
-import os
-import sys
-from PIL import Image
+import argparse
+import collections
 import math
+import os
+import re
 import shutil
+import sys
+
+from PIL import Image
 
 # Magic bytes, so we only try processing images
+sig_size = 16           # Maximum signature size to look for, in bytes
 magic_bytes = [
-    b'\xff\xd8',
-    b'\x89PNG\r\n\x1a'
+    b'\xff\xd8',        # JPEG
+    b'\x89PNG\r\n\x1a'  # PNG
 ]
+# TODO: Add WEBP/VP8 image support. Bytes 0-3 = 'RIFF', bytes 4-7 = variable (content len + 8?), 8-15 = 'WEBPVP8'
 
-# Desired aspect ratios
+
+""" # Desired aspect ratios
 desired_aspect = [
     (8, 5),     # 16:10
     (16, 9),    # 16:9
     (21, 9),    # 21:9
     (32, 9),    # 32:9
-]
+] """
 
-# Minimum acceptable resolution
-min_x = 1920
-min_y = 1080
 
-if len(sys.argv) < 3:
-    print(f"Usage: {sys.argv[0]} <dir to process> <dir to move acceptable files to>")
-    sys.exit(1)
+def setup() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('src', type=str, help='Source directory to process.')
 
-if not os.path.isdir(sys.argv[1]):
-    print(f"Source dir ({sys.argv[1]}) not found.")
-    sys.exit(2)
+    # Mode selector
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument('--dst', type=str, help='Target directory for all matched files.')
+    mode.add_argument('--list_sigs', action='store_true', help='Scan all files and list file signatures. Useful for adding missing types to magic_bytes list.')
 
-if not os.path.isdir(sys.argv[2]):
-    print(f"Target dir ({sys.argv[2]}) not found.")
-    sys.exit(4)
+    # Minimum resolution
+    parser.add_argument('--min_x', type=int, default=1920, help='Minimum resolution to include in results. (Default: 1920)')
+    parser.add_argument('--min_y', type=int, default=1080, help='Minimum resolution to include in results. (Default: 1080)')
 
-for root, dir_list, file_list in os.walk(sys.argv[1]):
-    for f in file_list:
-        path = os.path.join(root, f)
-        fd = open(path, 'rb')
-        startbytes = fd.read(32)
-        if any([startbytes.startswith(byte_pattern) for byte_pattern in magic_bytes]):
-            img = Image.open(fd)
-            x, y = img.size[0], img.size[1]
-            fd.close()
-            gcd = math.gcd(x, y)
-            aspect = (int(x/gcd), int(y/gcd))
-            if aspect in desired_aspect:
-                if x < min_x or y < min_y:
-                    print(f"Smaller than minimum resolution: {path} ({x}x{y})")
-                else:
-                    print(f"OK: {path} {aspect}")
-                    shutil.move(path, sys.argv[2])
-            else:
-                # Ignore files that don't fit our requirements
-                continue
+    parser.add_argument('--aspect', type=str, nargs='+', default=['16:9'], help='Aspect ratios to match, separated by spaces. (e.g.: "--aspect 16:10 32:9") If this is not specified, only 16:9 images will be matched by default.')
+    parser.add_argument('--copy_only', action='store_true', help='Used in conjunction with --dst, will not remove files from src.')
+    return parser.parse_args()
+
+
+def enumerate_files(path: str) -> list:
+    dirs = []
+    for root, _, file_list in os.walk(path):
+        for f in file_list:
+            dirs.append(os.path.join(root, f))
+    return dirs
+
+
+def get_sig(path) -> str:
+    with open(path, 'rb') as fd:
+        sig = fd.read(sig_size)
+    return sig
+
+
+# Main segment
+args = setup()
+
+# Mode switch
+# --dst logic
+if args.dst:
+    # Build aspect list
+    desired_aspect = []
+    for aspect in args.aspect:
+        if re.match('^\d+:\d+$', aspect) is None:
+            print(f"Invalid --aspect parameter provided: {aspect}")
+            sys.exit(1)
         else:
-            fd.close()
-            print(f"Unrecognized file signature: {startbytes} in {path}")
+            desired_aspect.append(tuple([int(i) for i in aspect.split(':')]))
+
+    # Build matched file list
+    matches = []
+    for path in enumerate_files(args.src):
+        sig = get_sig(path)
+        with open(path, 'rb') as fd:
+            if any([sig.startswith(byte_pattern) for byte_pattern in magic_bytes]):
+                img = Image.open(fd)
+                x, y = img.size[0], img.size[1]
+                gcd = math.gcd(x, y)
+                aspect = (int(x / gcd), int(y / gcd))
+                if aspect in desired_aspect:
+                    if x < args.min_x or y < args.min_y:
+                        print(f"Smaller than minimum resolution: {path} ({x}x{y})")
+                    else:
+                        print(f"OK: {path} {aspect}")
+                        matches.append(path)
+    # Relocate files as necessary
+    for path in matches:
+        # Strip the src dir from the matched file path, so we can create dirs as needed in dst.
+        filesubdir = os.path.split(path)[0][len(args.src):]
+        filename = os.path.split(path)[1]
+        if not os.path.isdir(os.path.join(args.dst, filesubdir)):
+            os.mkdir(os.path.join(args.dst, filesubdir))
+        
+        # Default operation is copy2 (data+metadata)
+        if not args.copy_only:
+            shutil.move(path, os.path.join(args.dst, filesubdir, filename))
+        else:
+            shutil.copy2(path, os.path.join(args.dst, filesubdir, filename))
+
+# --list_sigs logic
+elif args.list_sigs:
+    dict_file_sigs = collections.defaultdict(list)
+    for path in enumerate_files(args.src):
+        dict_file_sigs[get_sig(path)].append(path)
+    for sig in sorted(dict_file_sigs.keys()):
+        print(f"{sig} ({len(dict_file_sigs[sig])} entries):")
+        for path in sorted(dict_file_sigs[sig]):
+            print(path)
+        print('-' * 40)
+
+else:
+    print('???')
+    sys.exit(1)
